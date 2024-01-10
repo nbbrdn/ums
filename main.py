c3fb90c8a577a19db537a4d060e08871d493714c
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-01-10 12:19:07 +1000

    Add flask sessions

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ums.sqlite"
 app.config["SECRET_KEY"] = "change-me"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+Session(app)
 
 
 class User(db.Model):
@@ -36,9 +40,33 @@ def admin_index():
     return render_template("admin/index.html", title="Admin Panel")
 
 
-@app.route("/user/")
+@app.route("/user/", methods=["POST", "GET"])
 def user_index():
-    return render_template("user/index.html", title="User Panel")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User().query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            if user.status == 0:
+                flash("Your account is not approved by Admin", "danger")
+                return redirect("/user/")
+            else:
+                session["user_id"] = user.id
+                session["username"] = user.username
+                flash("Login Successfully", "success")
+                return redirect("/user/dashboard")
+        else:
+            flash("User email or password incorrect", "danger")
+            return redirect("/user/")
+    else:
+        return render_template("user/index.html", title="User Panel")
+
+
+@app.route("/user/dashboard")
+def user_dashboard():
+    if session.get("username"):
+        return f"{session.get('username')}"
 
 
 @app.route("/user/signup/", methods=["GET", "POST"])
